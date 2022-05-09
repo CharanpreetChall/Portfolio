98410607a49b14dbac149fff6b530e04b7930caa
Charanpreet Chall <charanpreetchall@gmail.com>
2022-05-09 16:48:24 -0400

    Removed unused tags

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.scss';
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter} from "react-router-dom";
 import Nav from './Components/Nav/Nav';
 import Hero from './pages/Hero/Hero';
 import Projects from './pages/Projects/Projects';
